61882f7bf6cfe1f2b801fa23432eb931b5e7e4e5
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-06-26 17:02:42 +0200

    Part of Iteration 2 made, shuffleCards missing

--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,9 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
@@ -9,7 +12,14 @@ class MemoryGame {
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2){
+      this.pairsGuessed += 1;
+      this.checkIfFinished();
+      return true;
+    }else{
+      return false;
+    }
   }
 
   checkIfFinished() {
